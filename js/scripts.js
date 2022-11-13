a9ba0f612142b2ae5e7e2476d31042890ec72ed7
JackOliCode <jack.v.oliver@gmail.com>
2022-11-13 20:12:35 +0000

    Added Ivysaur and Venusaur objects to pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']}
-
-
+    {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
+    {name: 'Ivysaur', height: 1.0, type: ['grass', 'poison']},
+    {name: 'Venusaur', height: 2, type: ['grass', 'poison']},
 
 
 ];
