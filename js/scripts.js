480f4429eb116888f3b425eec2f40ef72a51ff18
JackOliCode <jack.v.oliver@gmail.com>
2022-12-02 15:04:48 +0000

    Put pokemonList inside a IIFE pokemonRepository with the returns add and getAll

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+/* 
 let pokemonList = [
     {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
     {name: 'Ivysaur', height: 1.0, type: ['grass', 'poison']},
@@ -8,8 +9,10 @@ let pokemonList = [
 
 
 
-];
-// code for loop to list pokemon and height
+];/*
+
+// code for loop to list pokemon and height 
+/*
 for (let i=0; i < pokemonList.length; i++){
     document.write(`${pokemonList[i].name} (height ${pokemonList[i].height})`);
     if (pokemonList[i].height > 1.7) {
@@ -17,4 +20,31 @@ for (let i=0; i < pokemonList.length; i++){
     }else {
         document.write("<br>");
     }
-}
+} */
+
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
+        {name: 'Ivysaur', height: 1.0, type: ['grass', 'poison']},
+        {name: 'Venusaur', height: 2, type: ['grass', 'poison']},
+        {name: 'Charmander', height: 0.6, type: ['fire']},
+        {name: 'Charmeleon', height: 1.1 , type: ['fire']},
+        {name: 'Charizard', height: 1.7, type: ['fire', 'flying']}];
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+    
+    function getAll() {
+        return pokemonList
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add ({name: 'Pikachu', height: 1.4, type: ['electric'] });
+console.log(pokemonRepository.getAll());
