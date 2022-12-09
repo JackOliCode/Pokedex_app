4229c3c612dbab2f59c4bec5ce37d666f962eb6d
JackOliCode <jack.v.oliver@gmail.com>
2022-12-09 19:30:08 +0000

    deleted old code for earlier exercises

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,3 @@
-/*
-let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
-    {name: 'Ivysaur', height: 1.0, type: ['grass', 'poison']},
-    {name: 'Venusaur', height: 2, type: ['grass', 'poison']},
-    {name: 'Charmander', height: 0.6, type: ['fire']},
-    {name: 'Charmeleon', height: 1.1 , type: ['fire']},
-    {name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
-];*/
-
-// code for loop to list pokemon and height 
-/*
-for (let i=0; i < pokemonList.length; i++){
-    document.write(`${pokemonList[i].name} (height ${pokemonList[i].height})`);
-    if (pokemonList[i].height > 1.7) {
-        document.write(" - Wow, that's big! <br>");
-    }else {
-        document.write("<br>");
-    }
-} */
-
-
 // IIFE for pokemonList array
 let pokemonRepository = (function () {
     let pokemonList = [
