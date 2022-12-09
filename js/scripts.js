f97ddb05c4e327653c998e5c0118e3b8a3f4e2f3
JackOliCode <jack.v.oliver@gmail.com>
2022-12-09 20:02:49 +0000

    updated the forEach loop and set/created new variables to create list
    set variable for .pokemon-list
    created 2 elements; listItem & button
    set .innerText for button
    added class to button .button-class
    appended button to listItem
    appended listItem to pokemonul

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,7 @@ let pokemonRepository = (function () {
     };
 })();
 
-//update forEach loop using key to access IIFE
+/*update forEach loop using key to access IIFE
 pokemonRepository.getAll().forEach(function(pokemon) {
     document.write(`${pokemon.name} (height ${pokemon.height})`);
     if (pokemon.height > 1.7) {
@@ -30,4 +30,15 @@ pokemonRepository.getAll().forEach(function(pokemon) {
     }else {
         document.write("<br>");
     }
+});*/
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+    document.write(`${pokemon.name} (height ${pokemon.height})`);
+    let pokemonul = document.querySelector('.pokemon-list');
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = [pokemon.name];
+    button.classList.add('button-class');
+    listItem.appendChild(button);
+    pokemonul.appendChild(listItem);
 });
