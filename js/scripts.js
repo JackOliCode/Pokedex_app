8c9666862f5cb047b1faf4261fcbeb097d308274
JackOliCode <jack.v.oliver@gmail.com>
2022-12-13 00:46:30 +0000

    added loadList() function to js

--- js/scripts.js
+++ js/scripts.js
@@ -28,6 +28,23 @@ let pokemonRepository = (function () {
         console.log(pokemon);
     }
 
+
+    function loadList() { //name function
+        return fetch(apiUrl).then(function (response) { //return gives something to use, fetch(apiUrl) is a promise, the value of which is given to response if resovled
+           return response.json(); // return response parameter and parsed via .json
+        }).then(function (json) { //.json() is another promise and the parsed info given to json parameter
+            json.results.forEach(function (item) { //forEach fuction for json parameter (which is the pulled info from api) given to parameter item
+                let = pokemon = { // new object 
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                add(pokemon); // add object
+            });
+        }).catch(function (e) {
+            console.error(e);
+        })   
+    }
+
     return {
         add: add,
         getAll: getAll,
