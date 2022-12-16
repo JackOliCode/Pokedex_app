a69cf82a509b613bc2117e78b75ceac6590caa50
JackOliCode <jack.v.oliver@gmail.com>
2022-12-16 21:13:45 +0000

    copied fuction for creating model + title + content in JS and attaching t modal-container.
    Currently showing as undefined - text and title

--- js/scripts.js
+++ js/scripts.js
@@ -25,8 +25,34 @@ let pokemonRepository = (function () {
     }
 
     function showDetails(pokemon) {
-        loadDetails(pokemon).then(function () {
-            console.log(pokemon);
+        loadDetails(pokemon).then(function showDetails(pokemonName, text) {
+            let modalContainer = document.querySelector('#modal-container');
+
+            // Clear all existing modal content
+            modalContainer.innerHTML = '';
+
+            let modal = document.createElement('div');
+            modal.classList.add('modal');
+
+            // Add the new modal content
+            let closeButtonElement = document.createElement('button');
+            closeButtonElement.classList.add('modal-close');
+            closeButtonElement.innerText = 'Close';
+
+            let titleElement = document.createElement('h1');
+            titleElement.innerText = pokemonName;
+
+            let contentElement = document.createElement('p');
+            contentElement.innerText = text;
+
+            modal.appendChild(closeButtonElement);
+            modal.appendChild(titleElement);
+            modal.appendChild(contentElement);
+            modalContainer.appendChild(modal);
+
+
+
+            modalContainer.classList.add('is-visible');
         });
     }
 
